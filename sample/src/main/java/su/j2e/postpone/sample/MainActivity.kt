732f8aef20e9c8ed78cb7adf7eefc3ed40a76469
j2esu <ipegorovev@gmail.com>
2021-04-19 19:54:19 +0300

    Add temp example

--- sample/src/main/java/su/j2e/postpone/sample/MainActivity.kt
+++ sample/src/main/java/su/j2e/postpone/sample/MainActivity.kt
@@ -1,12 +1,32 @@
 package su.j2e.postpone.sample
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import su.j2e.postpone.postponeTransition
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val f = BlankFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, f)
+            .commit()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val f2 = BlankFragment()
+            supportFragmentManager.postponeTransition(f).beginTransaction()
+                .hide(f)
+                .add(R.id.container, f2)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .addToBackStack(null)
+                .setReorderingAllowed(true)
+                .commit()
+        }, 2000)
     }
 }
